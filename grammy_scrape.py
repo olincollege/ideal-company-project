21941570e904d35e38193f8ec190b724219fd83c
softdes <dpark0703@gmail.com>
2021-03-26 22:06:46 -0400

    Added comments to the code and changed variable names for clarity

--- grammy_scrape.py
+++ grammy_scrape.py
@@ -1,15 +1,25 @@
+## Import BeautifulSoup, requests, and pandas to convert
+## the scraped data into a CSV file.
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+## Get the wikipedia link
+## and find the identification of the table
 wiki_url = "https://en.wikipedia.org/wiki/Grammy_Award_for_Song_of_the_Year"
 table_name = "wikitable sortable"
 
+## Prepare the scrape by getting the access to the wikipedia page
+## and set up the BeautifulSoup to pull the data
 response = requests.get(wiki_url, auth=('user', 'pass'))
 soup = BeautifulSoup(response.text, 'html.parser')
 
+## Pull the data and save all the data into a list
 grammy_table = soup.find('table', {'class':table_name})
-data = pd.read_html(str(grammy_table))
-df = data[0]
+grammy_data = pd.read_html(str(grammy_table))
 
-# print(df["Winner(s)"][20]) for testing
+## Turn the list of data into a dataframe
+grammy_dataframe = grammy_data[0]
+
+## For testing purposes
+## print(grammy_dataframe["Winner(s)"][3])
