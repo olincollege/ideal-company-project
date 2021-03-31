104320f78dd10895dcebef940c907f53b3ae6ef3
softdes <dpark0703@gmail.com>
2021-03-31 14:35:23 -0400

    Removed Max's Extraneous Non-Working Code

--- company_type.py
+++ company_type.py
@@ -53,35 +53,3 @@ for company_name, company_link in dict_company_links.items():
     dict_company_links[company_name] = company_type
 
 print(dict_company_links)
-
-# TODO: remove commented code below?
-# thead = grammy_table.find('data-colname')
-# column_names = [th.text.strip() for th in thead.find_all('th')]
-
-# table_rows = grammy_table.find_all('tr')
-# link_data_by_row = []
-
-# for row in table_rows:
-#     row_data = []
-#     for td in row.find_all('td'):
-#         check_element = td.find('a')
-#         if check_element is not None:
-#             link = td.a['href']
-#             row_data.append(link)
-#         else: 
-#             no_link = ''.join(td.stripped_strings)
-#             if no_link =='':
-#                 no_link = None
-#             row_data.append(no_link)
-#     link_data_by_row.append(row_data)
-
-# link_dataframe = pd.DataFrame(link_data_by_row, columns = column_names)
-# print(link_dataframe)
-
-
-
-# works = pd.read_html(grammy_table, match = 'Works')
-# grammy_links = grammy_table.find_all('a')
-
-
-#print(grammy_links)
